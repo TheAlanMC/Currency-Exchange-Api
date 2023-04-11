e658b59c5690d346d0c7febc878ab71820cb3590
Chris Apaza <chrisalanapazaa@gmail.com>
2023-04-11 13:44:11 -0400

    migrated to micro service

--- currency-kt/src/main/kotlin/bo/edu/ucb/currencykt/CurrencyKtApplication.kt
+++ currency-kt/src/main/kotlin/bo/edu/ucb/currencykt/CurrencyKtApplication.kt
@@ -5,12 +5,12 @@ import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 class CurrencyKtApplication
 
 fun main(args: Array<String>) {
